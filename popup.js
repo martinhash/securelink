c930f72ac21d9b4d54ad9c1a9293987de4564c38
Martin Barria Joost <martinjoostw@gmail.com>
2020-08-28 18:11:19 -0600

    feat(loadings): and disabled buttons

--- popup.js
+++ popup.js
@@ -17,12 +17,18 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   function setCountAll(res) {
     clean();
-    $("#txtAll").removeClass("none");
-    if (res) {
-      $("#txtAll").html(`This page have ${res.count} Links`);
-    } else {
-      $("#txtAll").html(`This page not have links`);
-    }
+    disabledButtons();
+    $("#loadingAll").removeClass("none");
+    setTimeout(() => {
+      enabledButtons();
+      $("#loadingAll").addClass("none");
+      $("#txtAll").removeClass("none");
+      if (res) {
+        $("#txtAll").html(`This page have ${res.count} Links`);
+      } else {
+        $("#txtAll").html(`This page not have links`);
+      }
+    }, 1000);
   }
 
   //UNSECURES FUNCTION
@@ -33,12 +39,18 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   function setCountUnsecure(res) {
     clean();
-    $("#txtUnsecure").removeClass("none");
-    if (res) {
-      $("#txtUnsecure").html(`This page have ${res.count} Unsecure Links`);
-    } else {
-      $("#txtUnsecure").html(`This page not have links`);
-    }
+    disabledButtons();
+    $("#loadingUnsecure").removeClass("none");
+    setTimeout(() => {
+      enabledButtons();
+      $("#loadingUnsecure").addClass("none");
+      $("#txtUnsecure").removeClass("none");
+      if (res) {
+        $("#txtUnsecure").html(`This page have ${res.count} Unsecure Links`);
+      } else {
+        $("#txtUnsecure").html(`This page not have links`);
+      }
+    }, 1000);
   }
 
   //SECURES FUNCTION
@@ -49,12 +61,30 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   function setCountSecure(res) {
     clean();
-    $("#txtSecure").removeClass("none");
-    if (res) {
-      $("#txtSecure").html(`This page have ${res.count} Secure Links`);
-    } else {
-      $("#txtSecure").html(`This page not have links`);
-    }
+    disabledButtons();
+    $("#loadingSecure").removeClass("none");
+    setTimeout(() => {
+      enabledButtons();
+      $("#loadingSecure").addClass("none");
+      $("#txtSecure").removeClass("none");
+      if (res) {
+        $("#txtSecure").html(`This page have ${res.count} Secure Links`);
+      } else {
+        $("#txtSecure").html(`This page not have links`);
+      }
+    }, 1000);
+  }
+
+  function disabledButtons() {
+    $("#btnUnsecure").attr("disabled", true);
+    $("#btnSecure").attr("disabled", true);
+    $("#btnAll").attr("disabled", true);
+  }
+
+  function enabledButtons() {
+    $("#btnUnsecure").attr("disabled", false);
+    $("#btnSecure").attr("disabled", false);
+    $("#btnAll").attr("disabled", false);
   }
 
   //CLEAN ALL LINKER TEXTS
@@ -62,5 +92,8 @@ document.addEventListener("DOMContentLoaded", function () {
     $("#txtAll").addClass("none");
     $("#txtUnsecure").addClass("none");
     $("#txtSecure").addClass("none");
+    $("#loadingUnsecure").addClass("none");
+    $("#loadingSecure").addClass("none");
+    $("#loadingAll").addClass("none");
   }
 });
